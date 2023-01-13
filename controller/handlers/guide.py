02a6cdc365d13c9b356691d1201026145bf498a7
Maksim Vikhrov <maxvihr@yandex.ru>
2023-01-13 17:11:02 +0300

    Updated guide info

--- controller/handlers/guide.py
+++ controller/handlers/guide.py
@@ -1,35 +1,9 @@
 GUIDE = """
         Application supports next features:
-
-        --  command [list 'sort_mode' 'reverse'] allows you to get sorted (FMID, MarketName, average rating) of all markets in US.
-            Modes:
-                '1' <- Sorted by FMID
-                '2' <- Sorted by MarketName (default)
-                '3' <- Sorted by rating
-            Reverse:
-                '+' <- Reverse False (default)
-                '-' <- Reverse True
-            Examples:
-                ==> Enter a command: list
-                ==> Enter a command: list 1 +
-                ==> Enter a command: list 3 -
-            ;
-        --  command [find *('filter_mode' 'value')] allows you to put a few filters and get (FMID, MarketName) markets matched your request.
-            Filters:
-                1 <- Filtered by city
-                2 <- Filtered by state
-                3 <- Filtered by zip
-            Examples:
-                ==> Enter a command: find 1 Miami
-                ==> Enter a command: 1 Danville 2 Vermont
-                ==> Enter a command: find 3 70062 3 57064
-            ;
-        --  command [show 'FMID'] allows you to get full info about specific market.
-            ;
-        --  command [review 'FMID'] allows you to leave a rating and write review about specific market.
-            ;
-        --  command [exit] makes exit from application.
-
+            - [list] - shows all Farmers markets sorted by parametres;
+            - [find] - shows specified markets according to your entered searching request;
+            - [guide] - shows app instruction;
+            - [exit] - finish session;
         """
 
 
