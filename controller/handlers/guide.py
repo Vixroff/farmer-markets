8852c4fd7f9eae9b5e1a5a75f897dc89d25a5b8f
Maksim Vikhrov <maxvihr@yandex.ru>
2023-01-17 18:01:01 +0300

    guide updated by review

--- controller/handlers/guide.py
+++ controller/handlers/guide.py
@@ -2,6 +2,7 @@ GUIDE = """
         Application supports next features:
             - [list] - shows all Farmers markets sorted by parametres;
             - [find] - shows specified markets according to your entered searching request;
+            - [review] - allows to give feedback;
             - [guide] - shows app instruction;
             - [exit] - finish session;
         """
